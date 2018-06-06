d863187c3f857a7543d44dc0bfc4428c9982916e
Cameron Horejs <cameron.horejs@gmail.com>
2018-06-05 21:25:14 -0700

    Reverted blogs controller back to original

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,3 +1,4 @@
+
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
@@ -28,9 +29,11 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Your post is now live.' }
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,7 +44,7 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-         location: @blog }
+        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
        end
@@ -54,6 +57,7 @@ class BlogsController < ApplicationController
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
