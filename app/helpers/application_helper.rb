46130f343c559844476b1aadd4f0eb4e3478d540
Cameron Horejs <cameron.horejs@gmail.com>
2018-08-05 15:15:10 -0700

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
     content_tag(:p, greeting, class: "source-greeting")
    end
   end
+
+  def copyright_generator
+    HorejsViewTool::Renderer.copyright 'Cameron Horejs', 'All rights reserved'
+  end
 end
